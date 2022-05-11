1adacf5ecd830a530821182a341a276c05c3e31f
Drew Mahler <drewmahler@Drews-MacBook-Pro-2.local>
2022-05-11 16:28:09 -0700

    got some initial work going

--- programming2.py
+++ programming2.py
@@ -1,5 +1,32 @@
+
+
+from random import randint
+
+
+def nonattacking(rep):
+    '''Determine number of pairs of non-attacking queens'''
+    pairs = 0
+    for i in range(8):
+        for j in range(i+1, 8):
+            if not (rep[i] == rep[j] 
+            or int(rep[i]) == int(rep[j]) + (i - j)
+            or int(rep[j]) == int(rep[j]) - (i - j)):
+                pairs += 1
+    return pairs
+            
+class Board:
+    def __init__(self, rep):
+        self.rep = rep
+        self.fitness = nonattacking(self.rep)
+
+def generate_board():
+    """Generate a possible representation of the 8 queens problem"""
+    return Board(
+        "".join([str(randint(1, 8)) for _ in range(8)])
+    )
+
 def GAQueens(population_size, num_iterations):
-    return 1
+    initial_population = [generate_board() for _ in range(population_size)]
 
 if __name__ == '__main__':
     pop_size = [100]
