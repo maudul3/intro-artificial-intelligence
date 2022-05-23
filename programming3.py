ff02a0387af72feec11a7e5159966acef85fa45b
Drew Mahler <drewmahler@Drews-MacBook-Pro-2.local>
2022-05-22 19:24:46 -0700

    Programming 3 work so far

---
+++ programming3.py
@@ -0,0 +1,125 @@
+from random import random, randint, choice
+import numpy as np
+from itertools import permutations
+
+NUM_ROWS_COLS = 10
+POSSIBLE_ACTIONS = ['N', 'E', 'S', 'W', 'P']
+
+def create_grid():
+    return np.array([
+            ['0' if random() < 0.5 else '1' for _ in range(10) ] 
+            for _ in range(10)
+        ])
+
+def sense_state(current_row, current_col, grid):
+    state = ""
+    # Add current
+    state += grid[current_row][current_col]
+    
+    # Add north
+    if current_row - 1 >= 0:
+        state += grid[current_row - 1][current_col]
+    else:
+        state += '2'
+    # Add east
+    if current_col + 1 < NUM_ROWS_COLS:
+        state += grid[current_row][current_col + 1]
+    else:
+        state += '2'
+    # Add south
+    if current_row + 1 < NUM_ROWS_COLS:
+        state += grid[current_row + 1][current_col]
+    else:
+        state += '2'
+    # Add west
+    if current_col - 1 >= 0:
+        state += grid[current_row][current_col - 1]
+    else:
+        state += '2'
+    
+    return state
+
+def initialize_q():
+    '''
+    STATES: CNESW (Current, North, East, South, West)
+    STATE REP: {0: Empty, 1: Can, 2: Wall}
+    ACTIONS: { N, E, S, W, P (Pick up)}
+
+    Create a dictionary which maps states -> {actions: q-value}
+    '''
+    q = {}
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    for m in range(3):
+                        for a in POSSIBLE_ACTIONS:
+                            q[str(i) + str(j) + str(k) + str(l) + str(m) + a] = 0
+    return q
+
+def run_action(row,col,action, grid):
+    reward = 0
+    if action == 'N':
+        if row - 1 >= 0:
+            row = row - 1
+        else:
+            reward = -5
+    elif action == 'S':
+        if row + 1 < NUM_ROWS_COLS:
+            row = row + 1
+        else:
+            reward = -5
+    elif action == 'W':
+        if col - 1 >= 0:
+            col = col - 1
+        else:
+            reward = -5
+    elif action == 'S':
+        if col + 1 < NUM_ROWS_COLS:
+            col = col + 1 
+        else:
+            reward = -5
+    elif action == 'C':
+        if grid[row][col] == 0:
+            reward = -1
+        elif grid[row][col] == 1:
+            reward = 10
+            grid[row][col] = 0
+
+    return row,col,grid,reward
+
+
+def q_learning(num_actions, q_matrix, epsilon, eta=0.2):
+    grid = create_grid()
+    row, col = randint(0, 9), randint(0, 9)
+    total_reward = 0
+    for _ in range(num_actions):
+        current_state = sense_state(row, col, grid)
+        # Determine are state-action keys for this state
+        keys = [key for key in q_matrix.keys() if current_state in key]
+        if random() < epsilon: # randomly choose action
+            action = choice(POSSIBLE_ACTIONS)
+        else: # choose optimal action
+            max_key = None
+            max_value = -10000
+            for key in keys:
+                if q_matrix[key] > max_value:
+                    max_key = key
+                    max_value = q_matrix[key]
+            action = max_key[-1] # pull action state-action key
+        row,col,grid,reward = run_action(row,col,action,grid)
+        total_reward += reward
+
+            
+if __name__ == '__main__':
+    episodes = 500
+    actions = 200
+    epsilon_start = 0.1
+    epsilon = epsilon_start
+    q_matrix = initialize_q()
+    rewards = []
+    for ep in range(episodes):
+        if ep % 50 == 0:
+            epsilon -= epsilon_start * (episodes / (ep + 1))
+        q_matrix, total_reward = q_learning(actions, q_matrix, epsilon)
+        rewards.append(total_reward)
