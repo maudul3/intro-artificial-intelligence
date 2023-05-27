6e1258772f8e209194524f1fe9676e5ee7727efb
Drew Mahler <drewmahler@Drews-MBP-2.attlocal.net>
2023-05-26 17:33:24 -0700

    Update function signatures for programming3.py

--- programming3.py
+++ programming3.py
@@ -7,12 +7,29 @@ POSSIBLE_ACTIONS = ["N", "E", "S", "W", "P"]
 
 
 def create_grid():
+    """Generates a 10 x 10 grid with cans and empty spaces
+
+    Outputs:
+        np.array: 10 x 10 grid where 1 is a can, 0 is empty
+
+    """
     return np.array(
         [["0" if random() < 0.5 else "1" for _ in range(10)] for _ in range(10)]
     )
 
 
 def sense_state(current_row, current_col, grid):
+    """Senses the state of the current row and column
+
+    Inputs:
+        current_row (int): index of current row
+        current_col (int): index of current column
+        grid (np.array): 10 x 10 grid
+
+    Outputs:
+        str: state of current position either 0, 1, or 2
+
+    """
     state = ""
     # Add current
     state += grid[current_row][current_col]
@@ -47,7 +64,8 @@ def initialize_q():
     STATE REP: {0: Empty, 1: Can, 2: Wall}
     ACTIONS: { N, E, S, W, P (Pick up)}
 
-    Create a dictionary which maps states -> {actions: q-value}
+    Outputs:
+        dict[str -> float]: { state-actions -> q-value }
     """
     q = {}
     for i in range(3):
@@ -61,6 +79,17 @@ def initialize_q():
 
 
 def run_action(row, col, action, grid):
+    """Updates the grid and position of robot with specified action
+
+    Inputs:
+        row (int): index of current row
+        col (int): index of current column
+        action (str): specified action for robot
+        grid (np.array): 10 x 10 grid
+
+    Outputs:
+        int, int, np.array, int: new row, new column, new grid, local reward
+    """
     reward = 0
     if action == "N":
         if row - 1 >= 0:
@@ -93,6 +122,15 @@ def run_action(row, col, action, grid):
 
 
 def max_key_value(keys, q):
+    """Determine the max q-value for the given keys
+
+    Inputs:
+        keys (list[str]): all actions for current state
+        q (dict[str -> float]): q-matrix that maps all state-action pairs to a q-value
+
+    Outputs:
+        str, float: action to take, q value associated with this action
+    """
     max_key = None
     max_value = -10000
     for key in keys:
@@ -104,6 +142,18 @@ def max_key_value(keys, q):
 
 
 def q_learning(num_actions, q_matrix, epsilon, eta=0.2, gamma=0.9):
+    """Runs an instance of q-learning
+
+    Inputs:
+        num_actions (int): total number of actions robot can take
+        q_matrix (dict[str -> float]): initial q matrix for grid
+        epsilon (float): term dictating whether or not to follow optimal action
+        eta (float): learning rate, determines how much new information overrides old
+        gamma (float): discount factor, determines importance of future rewards
+
+    Outputs:
+        dict[str -> float], float: q_matrix, total_reward for the run
+    """
     grid = create_grid()
     row, col = randint(0, 9), randint(0, 9)
     total_reward = 0
